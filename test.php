72b52670d7f42a40c64c854a3224a562ca14692b
Ha Anh Son <hason61vn@gmail.com>
2014-03-24 17:18:59 +0700

    test

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by SonHA
+ * User: Son Ha Anh (sonhaanh@vccorp.vn)
+ * Date: 3/24/14
+ * Time: 5:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+echo 'test';
