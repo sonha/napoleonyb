25e6abe27cc9e7359063d66bad9d24eb93bbf8ea
Ha Anh Son <hason61vn@gmail.com>
2014-03-26 15:59:33 +0700

    Delete Draft file

--- sohagame.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by SonHA
- * User: Son Ha Anh (sonhaanh@vccorp.vn)
- * Date: 3/25/14
- * Time: 2:32 PM
- * To change this template use File | Settings | File Templates.
- */
-echo 'sohagame';
